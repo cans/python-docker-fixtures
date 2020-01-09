386451c77e8ee80af9e98c286d48dfae7563e00d
Nicolas CANIART <nicolas@caniart.net>
2020-01-09 01:58:38 +0100

    Image catalog fixes.
    Fix typo in postgres image name;
    Add default environment for the all-in-one Kafka image;

--- src/dockerfixtures/catalog.py
+++ src/dockerfixtures/catalog.py
@@ -13,7 +13,7 @@ __PG_ENV = {'POSTGRES_PASSWORD': '',
             }
 PG_12 = Image('postgres', tag='12', environment=__PG_ENV)
 PG_12_ALPINE = Image('postgres', tag='12-alpine', environment=__PG_ENV)
-PG_11 = Image('postgresql', tag='11', environment=__PG_ENV)
+PG_11 = Image('postgres', tag='11', environment=__PG_ENV)
 PG_11_ALPINE = Image('postgres', tag='11-alpine', environment=__PG_ENV)
 PG_10 = Image('postgres', tag='10', environment=__PG_ENV)
 PG_10_ALPINE = Image('postgres', tag='10-alpine', environment=__PG_ENV)
@@ -26,7 +26,10 @@ __KAFKA_ENV = {'KAFKA_ADVERTISED_LISTENERS': 'LISTENERS_DOCKER_INTERNAL://${DOCK
                }
 CONFLUENT_KAFKA_5_0_0 = Image('confluentinc/kafka', tag='5.0.0', environment=__KAFKA_ENV)
 
-__PAPERLIB_KAFKA_ENV = {}
+__PAPERLIB_KAFKA_ENV = {'ADVERTISED_HOST': '127.0.0.1',
+                        'KAFKA_HEAP_OPTS': '-Xmx256M -Xms256M',
+                        'ZOOKEEPER_HEAP_OPTS': '-Xmx128M -Xms128M',
+                        }
 PAPERLIB_KAFKA_LATEST = Image('paperlib/kafka', tag='latest', environment=__PAPERLIB_KAFKA_ENV)
 PAPERLIB_KAFKA_2_3_1 = Image('paperlib/kafka', tag='2.3.1', environment=__PAPERLIB_KAFKA_ENV)
 PAPERLIB_KAFKA_2_3_1_ALPINE = Image('paperlib/kafka', tag='2.3.1-alpine', environment=__PAPERLIB_KAFKA_ENV)
