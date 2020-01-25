9a54a39d84b193e1aed1c8dd02d2f1b63443b42e
Nicolas CANIART <nicolas@caniart.net>
2020-01-25 18:16:45 +0100

    Add test to show example works

--- tests/docker/conftest.py
+++ tests/docker/conftest.py
@@ -4,6 +4,8 @@ import logging
 import docker
 import pytest
 
+from dockerfixtures import container, image
+
 
 # Set log leve to DEBUG to get some logs from the docker lib
 logging.getLogger().setLevel(logging.DEBUG)
@@ -12,6 +14,20 @@ logging.getLogger().setLevel(logging.DEBUG)
 @pytest.fixture
 def client():
     return docker.from_env()
+
+
+@pytest.fixture
+def netcat_port():
+    return 9876
+
+
+@pytest.fixture(scope='function')
+def netcat(netcat_port):
+    client = docker.from_env()
+    yield from container.fixture(client,
+                                 image.Image('subfuzion/netcat'),
+                                 command=['nc', '-vl', str(netcat_port)],
+                                 )
 
 
 # vim: et:sw=4:syntax=python:ts=4:
