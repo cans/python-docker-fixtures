8686285507628aa2ee4951b36a3cee38aebf92b8
Nicolas CANIART <nicolas@caniart.net>
2020-01-26 00:30:28 +0100

    Fix build workflow
    Properly separate the build step from the publication
    step of the package.

--- .circleci/package-build.sh
+++ .circleci/package-build.sh
@@ -18,23 +18,3 @@ printf '\033[1;32m****************************************************\033[0m\n'
 printf "Package version: "
 cat /tmp/workspace/version
 printf '\033[1;32m****************************************************\033[0m\n'
-
-# Only upload non-developement packages (which name does not contain a '+' sign)
-if grep -vl '+' /tmp/workspace/version > /dev/null
-then
-    cat > ~/.pypirc <<HERE
-[distutils]
-index-servers =
-    pypi
-
-[pypi]
-repository = ${1}
-username = __token__
-password = ${2}
-HERE
-
-    twine upload -r pypi dist/*.whl
-    rm ~/.pypirc
-else
-    printf '\033[0;31mNot uploading the package, it appears to be a development one.\033[0m\n'
-fi
